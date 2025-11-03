f33cbd6d77532068bc7297a8c4a22b1a9ab7e60c
Danik <momentssd@gmail.com>
2025-11-03 17:22:31 +0300

    A3.py
    Изменил названия x и y на pred и tek чтобы не путаться. Также изменил cost на price что более логичнее. Добавил еще пару комментариев для понимания)

--- Lab 3/A3.py
+++ Lab 3/A3.py
@@ -1,20 +1,20 @@
-x = int(input()) #предыдущее
-y = int(input()) #текущее
-if y >= x:
-    used = y - x
+pred = int(input()) #предыдущее
+tek = int(input()) #текущее
+if tek >= pred:
+    used = tek - pred #счетчик сбросился
 else:
-    used = (10000 - x) + y
+    used = (10000 - pred) + tek
 if used <= 300:
-    cost = 21.0
+    prise = 21.0
 elif used <= 600:
-    cost = 21 + (used - 300) * 0.06
+    prise = 21 + (used - 300) * 0.06
 elif used <= 800:
-    cost = 21 + 300 * 0.06 + (used - 600) * 0.04
+    prise = 21 + 300 * 0.06 + (used - 600) * 0.04
 else:
-    cost = 21 + 300 * 0.06 + 200 * 0.04 + (used - 800) * 0.025
+    prise = 21 + 300 * 0.06 + 200 * 0.04 + (used - 800) * 0.025
 avg_price = cost / used
-print("Предыдущее:", x)
-print("Текущее:", y)
+print("Предыдущее:", pred)
+print("Текущее:", tek)
 print("Использовано:", used)
-print("К оплате:", round(cost, 2))
-print("Ср. цена m^3:", round(avg_price, 2))
+print("К оплате:", round(prise, 2))
+print("Ср. цена:", round(avg_price, 2))
